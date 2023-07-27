9adb3502216cb0825590671dc7d497bd9da76c17
Ashley <ashley.e.shultz@gmail.com>
2023-07-27 16:47:52 -0700

    first commit

---
+++ packages/lib_a/lib_a/__init__.py
@@ -0,0 +1,7 @@
+from lib_b import func_b
+
+def func_a():
+    print('lib a func a')
+    func_b()
+
+func_a()
---
+++ packages/lib_b/lib_b/__init__.py
@@ -0,0 +1,5 @@
+from lib_c import func_c
+
+def func_b():
+    print('func b')
+    func_c()
